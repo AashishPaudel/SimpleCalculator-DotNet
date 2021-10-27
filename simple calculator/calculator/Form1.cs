3d33149d5151551a59f20b23f24829911f489751
AashishPaudel <67449396+AashishPaudel@users.noreply.github.com>
2021-10-27 13:26:09 +0545

    Add files via upload

---
+++ simple calculator/calculator/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double first_num = Convert.ToDouble(textBox1.Text);
+            double second_num = Convert.ToDouble(textBox2.Text);
+            double sum = first_num + second_num;
+            textBox3.Text = sum.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double first_num = Convert.ToDouble(textBox1.Text);
+            double second_num = Convert.ToDouble(textBox2.Text);
+            double substraction = first_num - second_num;
+            textBox3.Text = substraction.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double first_num = Convert.ToDouble(textBox1.Text);
+            double second_num = Convert.ToDouble(textBox2.Text);
+            double multiplication = first_num * second_num;
+            textBox3.Text = multiplication.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double first_num = Convert.ToDouble(textBox1.Text);
+            double second_num = Convert.ToDouble(textBox2.Text);
+            double division = first_num / second_num;
+            textBox3.Text = division.ToString();
+        }
+    }
+}
